3cb9a50dd566d891bfebd5adebd3501c824f96bb
PGWSUnity <pawlo9666@gmail.com>
2020-11-05 09:27:29 +0100

    Kolejne zadania wykonane
    Lekcja 4 Zadania 12345

---
+++ Assets/Scripts/Lesson4/Zadanie1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Zadanie1 : MonoBehaviour
+{
+    List<Vector3> positions = new List<Vector3>();
+    public float delay = 3.0f;
+    int objectCounter = 0;
+    public int numberOfObjects;
+    // obiekt do generowania
+    public GameObject block;
+    public GameObject triangle;
+    public GameObject obstacle;
+    public GameObject plate;
+    public GameObject ciasto;
+    public List<GameObject> lista = new List<GameObject>();
+
+
+    void Start()
+    {
+        // w momecie uruchomienia generuje 10 kostek w losowych miejscach
+        List<int> pozycje_x = new List<int>(Enumerable.Range(-9, 19).OrderBy(x => Guid.NewGuid()).Take(numberOfObjects));
+        List<int> pozycje_z = new List<int>(Enumerable.Range(-9, 19).OrderBy(x => Guid.NewGuid()).Take(numberOfObjects));
+       
+        lista.Add(block);
+        lista.Add(triangle);
+        lista.Add(obstacle);
+        lista.Add(plate);
+        lista.Add(ciasto);
+
+        for (int i = 0; i < numberOfObjects-1; i++)
+        {
+            this.positions.Add(new Vector3(pozycje_x[i], 5, pozycje_z[i]));
+        }
+        foreach (Vector3 elem in positions)
+        {
+            Debug.Log(elem);
+        }
+        // uruchamiamy coroutine
+        StartCoroutine(GenerujObiekt());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator GenerujObiekt()
+    {
+        Debug.Log("wywołano coroutine");
+        foreach (Vector3 pos in positions)
+        {
+            int counter = UnityEngine.Random.Range(0,5);
+            Instantiate(this.lista.ElementAt(counter), this.positions.ElementAt(this.objectCounter++), Quaternion.identity);
+            yield return new WaitForSeconds(this.delay);
+        }
+        // zatrzymujemy coroutine
+        StopCoroutine(GenerujObiekt());
+    }
+}
---
+++ Assets/Scripts/Lesson4/Zadanie4.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zadanie4 : MonoBehaviour
+{
+    // ruch wokół osi Y będzie wykonywany na obiekcie gracza, więc
+    // potrzebna nam referencja do niego (konkretnie jego komponentu Transform)
+    public Transform player;
+
+    public float sensitivity = 200f;
+    float xRotation = 0f;
+
+    void Start()
+    {
+        // zablokowanie kursora na środku ekranu, oraz ukrycie kursora
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // pobieramy wartości dla obu osi ruchu myszy
+        float mouseXMove = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseYMove = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        // wykonujemy rotację wokół osi Y
+        player.Rotate(Vector3.up * mouseXMove);
+
+        xRotation -= mouseYMove;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // a dla osi X obracamy kamerę dla lokalnych koordynatów
+        // -mouseYMove aby uniknąć ofektu mouse inverse
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(new Vector3(-mouseYMove, 0f, 0f), Space.Self);
+
+    }
+}
